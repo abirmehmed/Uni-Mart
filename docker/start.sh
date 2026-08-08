6dbea423d6e5ec17e4921941f3bb991de1dda06c
Abir Mehmed <abirmehmed@gmail.com>
2026-08-08 20:02:06 +0600

    Add diagnostic product/user count on boot

--- docker/start.sh
+++ docker/start.sh
@@ -3,6 +3,7 @@ set -e
 
 php artisan config:clear
 php artisan migrate:fresh --seed --force
+php artisan tinker --execute="echo 'Products: ' . App\Models\Product::count() . ', Users: ' . App\Models\User::count() . PHP_EOL;"
 
 php artisan reverb:start --host=0.0.0.0 --port=6001 &
 php artisan queue:work &
