25f1f3fe990fd5efc891142c76e47c1f0c4f7c02
Abir Mehmed <abirmehmed@gmail.com>
2026-08-09 20:13:47 +0600

    Start Reverb before seeding so broadcast events on create don't fail boot

--- docker/start.sh
+++ docker/start.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 set -e
-
 php artisan config:clear
+
+php artisan reverb:start --host=0.0.0.0 --port=6001 &
+sleep 2
+
 php artisan migrate:fresh --seed --force
 php artisan tinker --execute="echo 'Products: ' . App\Models\Product::count() . ', Users: ' . App\Models\User::count() . PHP_EOL;"
 
-php artisan reverb:start --host=0.0.0.0 --port=6001 &
 php artisan queue:work &
 php artisan serve --host=0.0.0.0 --port=8000 &
-
 exec caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
